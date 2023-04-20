f70e0ecc16032708b3a48a4390e260884406d284
Abu Taher <writerabuthaer@gmail.com>
2023-04-20 09:30:08 +0600

    push all code

---
+++ case.js
@@ -0,0 +1,42 @@
+function updateCaseNumber(isIncrease) {
+    const caseNumberField = document.getElementById("case-number-field");
+    const caseNumberString = caseNumberField.value;
+    const PreviousCaseNumber = parseInt(caseNumberString);
+
+    let newCaseNumber;
+
+    if (isIncrease === true) {
+        newCaseNumber = PreviousCaseNumber + 1;
+    }
+    else {
+        newCaseNumber = PreviousCaseNumber - 1;
+    }
+
+    caseNumberField.value = newCaseNumber;
+
+    return newCaseNumber
+}
+
+function updateCaseTotalPrice(newCaseNumber) {
+    const caseTotalPrice = newCaseNumber * 59;
+    const caseTotalElement = document.getElementById("case-total");
+
+    caseTotalElement.innerText = caseTotalPrice;
+}
+
+document.getElementById("btn-case-plus").addEventListener("click", function () {
+    const newCaseNumber = updateCaseNumber(true);
+
+    updateCaseTotalPrice(newCaseNumber);
+
+    calculateSubTotal();
+})
+
+
+document.getElementById("btn-case-minus").addEventListener("click", function () {
+    const newCaseNumber = updateCaseNumber(false);
+
+    updateCaseTotalPrice(newCaseNumber);
+
+    calculateSubTotal();
+})
